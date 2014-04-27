2844dd22a81efe70e78e6ff8e27eb9bf8ee79c5d
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-04-27 15:59:35 -0500

    Added Grunt build and Bower install features
    Added Grunt to build concatenated AMD module (minified or not) to deploy
    - to `release/amd` directory
    - to `/demos/webroot/assets/vendor/angular-logDecorator/`
    Added NPM package JSON for Grunt dependencies
    Added Bower files for installs of ng.logDecorator as angular plugin

---
+++ Gruntfile.js
@@ -0,0 +1,163 @@
+module.exports = function(grunt) {
+
+    /**
+     * Load required Grunt tasks. These are installed based on the versions listed
+     * in `package.json` when you do `npm install` in this directory.
+     */
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+    var buildDir = "/release";
+    var devDir   = "/src";
+
+    /**
+     * This is the configuration object Grunt uses to give each plugin its
+     * instructions.
+     */
+    var taskConfig = {
+
+        /**
+         * We read in our `package.json` file so we can access the package name and version. It's already there, so
+         * we don't repeat ourselves here.
+         */
+        pkg: grunt.file.readJSON("package.json"),
+
+        /**
+         * The banner is the comment that is placed at the top of our compiled source files. It is first processed
+         * as a Grunt template, where the `<%=` pairs are evaluated based on this very configuration object.
+         */
+        meta: {
+            banner: '/**\n' +
+                ' * @appName    <%= pkg.name %>\n' +
+                ' * @version    <%= pkg.version %>\n' +
+                ' * @date       <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                ' * @homepage   <%= pkg.homepage %>\n' +
+                ' * @copyright  <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+                ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
+                ' */\n'
+        },
+
+         /**
+         * The directories to delete when `grunt clean` is executed.
+         */
+        clean: {
+            src: [
+                '<%= buildDir %>/*.js'
+            ],
+            options: {
+                force: true
+            }
+        },
+
+        /**
+         * The `copy` task just copies files from A to B. We use it here to copy our project assets
+         * (images, fonts, etc.) and javascripts into `buildDir`, and then to copy the assets to `compileDir`.
+         */
+        copy: {
+            release: {
+                files: [
+                    {
+                        src: './release/angular-logDecorator.js',
+                        dest: './demos/webroot/assets/vendor/angular-logDecorator/angular-logDecorator.js'
+                    }
+                ]
+            },
+            releaseMin: {
+                files: [
+                    {
+                        src: './release/angular-logDecorator.min.js',
+                        dest: './demos/webroot/assets/vendor/angular-logDecorator/angular-logDecorator.min.js'
+                    }
+                ]
+            }
+        },
+
+        /**
+         * Minifies RJS files and makes it production ready
+         * Build files are minified and encapsulated using RJS Optimizer plugin
+         */
+        requirejs: {
+            compile: {
+
+                options: {
+                    baseUrl: "./src",
+                    paths   :
+                    {
+                        // Configure alias to full paths; relative to `baseURL`
+                        'logger': 'mindspace/logger',
+                        'utils' : 'mindspace/utils'
+
+                    },
+                    out: './release/amd/angular-logDecorator.js',
+                    name: 'angular-logDecorator',
+
+                    preserveLicenseComments: true,
+                    optimize: "none"
+                }
+            },
+            compileMin: {
+
+                options: {
+                    baseUrl: "./src",
+                    paths   :
+                    {
+                        // Configure alias to full paths; relative to `baseURL`
+                        'logger': 'mindspace/logger',
+                        'utils' : 'mindspace/utils'
+
+                    },
+                    out: './release/amd/angular-logDecorator.min.js',
+                    name: 'angular-logDecorator',
+
+                    preserveLicenseComments: false,
+                    optimize: "uglify"
+                }
+            }
+        },
+
+        bump: {
+            scripts: {
+                files: ["src/*.js"],
+                    updateConfigs: ["pkg"],
+                    commitFiles: ["-a"],
+                    push: false
+            }
+        }
+
+
+
+    };
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Initialize with configuration
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    grunt.initConfig( taskConfig );
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Register Tasks
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    grunt.registerTask( "release", [
+
+          'clean:src'
+        , "requirejs:compile"    // concatenate and do NOT minify all the logDecorator source AMDs
+        , "copy:release"
+    ]);
+
+
+    grunt.registerTask( "release.min", [
+
+          "requirejs:compileMin" // concatenate and minify all the logDecorator source AMDs
+        , "copy:releaseMin"
+    ]);
+
+    grunt.registerTask('default', ['release','release.min']);
+
+
+};
